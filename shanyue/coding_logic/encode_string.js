f942db561f95258ae4bbc24bfff5be30574f2e07
fwio <624835339@qq.com>
2022-03-27 22:46:10 +0800

    对字符串中连续出现的字符进行压缩编码

---
+++ shanyue/coding_logic/encode_string.js
@@ -0,0 +1,16 @@
+// 对以下字符串进行压缩编码
+// Input: 'aaaabbbccd'
+// Output: 'a4b3c2d1'，代表 a 连续出现四次，b连续出现三次，c连续出现两次，d连续出现一次
+function encode(str){
+  const pattern = /([a-zA-Z])\1+/g
+  return str.replace(pattern, (match, p1) => `${p1}${match.length}`)
+}
+// test
+//=> a4b3c2
+console.log(encode('aaaabbbcc'))
+
+//=> a4b3a4
+console.log(encode('aaaabbbaaaa'))
+
+//=> a2b2c2
+console.log(encode('aabbcc'))
